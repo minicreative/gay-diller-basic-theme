7c6c732eba45ae7dc567cd6cfa2cb8515b841354
Tomas Roy <hello@minicreative.net>
2019-03-18 21:02:27 -0400

    head.php removed from typical repo

---
+++ starter-files/head.php
@@ -0,0 +1,3 @@
+<link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Lato:400,700" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/plugins/slick/slick.css" rel="stylesheet">
+<script src="<?php echo get_template_directory_uri(); ?>/plugins/slick/slick.min.js"></script>
